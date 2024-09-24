559e6ab7dd8e5329ba522477c046c724e819e242
santiagov2004 <santiagovargad2004@gmail.com>
2024-09-24 09:19:57 -0500

    Bd finalizada

--- user/admin.py
+++ user/admin.py
@@ -1,24 +1,7 @@
 from django.contrib import admin
-from .models import User, Proyecto, Roles, MiembrosProyectos, Archivos, Comentarios
+from .models import User
 
 # Register your models here.
 
 admin.site.register(User)
 
-@admin.register(Proyecto)
-class ProyectoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'fecha_inicio', 'fecha_fin', 'progreso')
-
-admin.site.register(Roles)
-
-@admin.register(MiembrosProyectos)
-class MiembrosProyectosAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'proyecto', 'rol')
-
-@admin.register(Archivos)
-class ArchivosAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'proyecto', 'usuario', 'archivoss')
-
-@admin.register(Comentarios)
-class ComentariosAdmin(admin.ModelAdmin):
-    list_display = ('comentario', 'proyecto', 'usuario', 'fecha_hora')
