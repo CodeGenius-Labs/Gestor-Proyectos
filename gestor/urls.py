1facf07f8bf48d05809a3e683ddcc45ef90b02df
edwin <edwinbustosg.com>
2024-10-22 06:38:48 -0500

    completo

--- gestor/urls.py
+++ gestor/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('superadmin/', views.superadmin, name='superadmin'),
     path('superproyecto/', views.superproyecto, name='superproyecto'),
     path('superusuario/', views.superusuario, name='superusuario'),
+    path('crear_proyecto/', views.crear_proyecto, name='crear_proyecto'),
     
     
 
