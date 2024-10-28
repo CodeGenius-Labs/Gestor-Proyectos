4c5cd02676ae14b72c8727897d8f7bebf4acbd1d
JeinnertSalazarM <stevensalazsr@gmail.com>
2024-10-28 14:25:20 -0500

    Responsive,sidebar en progreso unu, y aja

--- gestor/static/js/modalcrearusu.js
+++ gestor/static/js/modalcrearusu.js
@@ -7,7 +7,13 @@
                 document.getElementById('email').value = this.getAttribute('data-email');
             });
 
-            
+            document.getElementById('btn-crear-usuario-side').addEventListener('click', function() {
+                document.getElementById('modalsu').style.display = 'block';
+
+                document.getElementById('user_id').value = this.getAttribute('data-id');
+                document.getElementById('username').value = this.getAttribute('data-username');
+                document.getElementById('email').value = this.getAttribute('data-email');
+            });
             document.getElementById('closeModalsuBtn').addEventListener('click', function() {
                 document.getElementById('modalsu').style.display = 'none';
             });
