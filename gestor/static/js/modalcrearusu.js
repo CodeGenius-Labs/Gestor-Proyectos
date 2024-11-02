341c68b1a28a065247ce49554f77965a52552445
JeinnertSalazarM <stevensalazsr@gmail.com>
2024-11-01 21:54:53 -0500

    assdas

--- gestor/static/js/modalcrearusu.js
+++ gestor/static/js/modalcrearusu.js
@@ -5,12 +5,18 @@
                 document.getElementById('username').value = this.getAttribute('data-username');
                 document.getElementById('email').value = this.getAttribute('data-email');
             });
+
+
             document.getElementById('btn-crear-usuario-side').addEventListener('click', function() {
                 document.getElementById('modalsu').style.display = 'block';
                 document.getElementById('user_id').value = this.getAttribute('data-id');
                 document.getElementById('username').value = this.getAttribute('data-username');
                 document.getElementById('email').value = this.getAttribute('data-email');
             });
+
+
+
+            
             window.onclick = function(event) {
                 var modal = document.getElementById('modalsu');
                 if (event.target == modal) {
